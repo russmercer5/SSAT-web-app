16a1696eaf9e7ae17217fb8a5599c8b21e6c4c3f
russmercer5 <russmercer5@gmail.com>
2026-06-15 10:34:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ if ON_RENDER:
         # user[0] is the is_admin value
         return user[0] if user else False
     
-    def create_user(username, password, is_admin=True):
+    def create_user(username, password, is_admin=False):
         """Create a new user"""
         conn = get_db_connection()
         cursor = conn.cursor()
